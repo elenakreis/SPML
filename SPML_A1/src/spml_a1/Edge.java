501ecd8cb551d047061d1cbc00d5635cb74c1601
s4841670 <s4841670@TUVOK.b-fac.ru.nl>
2018-03-01 14:07:34 +0100

    Edge class added

---
+++ SPML_A1/src/spml_a1/Edge.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spml_a1;
+
+/**
+ *
+ * @author s4841670
+ */
+public class Edge {
+    
+}
