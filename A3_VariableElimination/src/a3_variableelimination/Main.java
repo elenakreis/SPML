ec30e2519d69d4e57920b7ed605373f2ad8fff07
Elena <elenakreis@web.de>
2018-05-16 18:52:09 +0200

    Imported pregiven code

--- A3_VariableElimination/src/a3_variableelimination/Main.java
+++ A3_VariableElimination/src/a3_variableelimination/Main.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a3_variableelimination;
 
-/**
- * @author Isabel Burgos s4718321
- * @author Elena Kreis s4841670
- */
-public class Main {
+import java.util.ArrayList;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Leggooo");
-    }
-    
+/**
+ * Main class to read in a network, add queries and observed variables, and
+ * eliminate variables.
+ * 
+ * @author Marcel de Korte, Moira Berens, Djamari Oetringer, Abdullahi Ali, Leonieke van den Bulk
+ */
+
+public class Main {
+	private final static String networkName = "earthquake.bif"; // The network to be read in (format from http://www.bnlearn.com/bnrepository/)
+
+	public static void main(String[] args) {
+		
+		// Read in the network
+		Networkreader reader = new Networkreader(networkName); 
+		
+		// Get the variables and probabilities of the network
+		ArrayList<Variable> Vs = reader.getVs(); 
+		ArrayList<Table> Ps = reader.getPs(); 
+		
+		// Print variables and probabilities
+		reader.printNetwork(Vs, Ps);
+
+		// Ask user for query and heuristic
+		reader.askForQuery(); 
+		
+		// Turn this on if you want to experiment with different heuristics for bonus points (you need to implement the heuristics yourself)
+		//reader.askForHeuristic();
+		//String heuristic = reader.getHeuristic();
+		
+		Variable Q = reader.getQueriedVariable(); 
+		
+		// Ask user for observed variables 
+		reader.askForObservedVariables(); 
+		ArrayList<Variable> O = reader.getObservedVariables(); 
+		
+		// Print the query and observed variables
+		reader.printQueryAndObserved(Q, O); 
+		
+		
+		//PUT YOUR CODE FOR THE VARIABLE ELIMINATION ALGORITHM HERE
+	}
 }
---
+++ A3_VariableElimination/src/a3_variableelimination/Table.java
@@ -0,0 +1,69 @@
+package a3_variableelimination;
+
+import java.util.ArrayList;
+
+/**
+ * Class to represent the Table Object consisting of probability rows
+ * 
+ * @author Marcel de Korte, Moira Berens, Djamari Oetringer, Abdullahi Ali, Leonieke van den Bulk
+ */
+
+public class Table {
+
+	private ArrayList<ProbRow> table;
+	private Variable node;
+	private ArrayList<Variable> parents;
+	
+	/**
+	 * Constructor of the class.
+	 * @param table made out of probability rows (ProbRows)
+	 * @param node belonging to the current probability table
+	 * @param parents belonging to the current probability table
+	 */
+	public Table(ArrayList<ProbRow> table, Variable node, ArrayList<Variable> parents) {
+		this.table = table;
+		this.node = node;
+		this.parents = parents;
+	}
+
+	/**
+	 * Getter of the table made out of ProbRows
+	 * @return table
+	 */
+	public ArrayList<ProbRow> getTable() {
+		return table;
+	}
+	
+	 /**
+	 * Getter of the node that belongs to the probability table
+	 * @return the node
+	 */
+	public Variable getNode() {
+		return node;
+	}
+
+	/**
+	 * Getter of the parents that belong to the probability table
+	 * @return the parents
+	 */
+	public ArrayList<Variable> getParents() {
+		return parents;
+	}
+
+	/**
+	  * Gets the i'th element from the ArrayList of ProbRows
+	  * @param i index
+	  * @return i'th ProbRow in Table
+	  */
+	public ProbRow get(int i) {
+		return table.get(i);
+	}
+	
+	/**
+	 * Returns the size of the Table (amount of probability rows)
+	 * @return size of Table
+	 */
+	public int size() {
+		return table.size();
+	}
+}
