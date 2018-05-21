ff5a9111053e876803f3dd5bb445362a5512df0b
Elena <elenakreis@web.de>
2018-05-21 11:20:38 +0200

    finished e), started f)

--- A3_VariableElimination/src/a3_variableelimination/Main.java
+++ A3_VariableElimination/src/a3_variableelimination/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Networkreader reader = new Networkreader(networkName);
 
         // Get the variables and probabilities of the network
-        ArrayList<Variable> Vs = reader.getVs();        
+        ArrayList<Variable> Vs = reader.getVs();
         ArrayList<Table> Ps = reader.getPs();
 
         // Print variables and probabilities
@@ -34,7 +34,7 @@ public class Main {
         Variable Q = reader.getQueriedVariable();
 
         // Ask user for observed variables 
-        reader.askForObservedVariables();        
+        reader.askForObservedVariables();
         ArrayList<Variable> O = reader.getObservedVariables();
 
         // Print the query and observed variables
@@ -48,35 +48,78 @@ public class Main {
         // a) done
         // b) done
         // c) done
-        // d) factorizing done; todo: reduce observed variables
+        // d) factorizing done; 
+        //    todo: reduce observed variables
         if (O.size() > 0) {
             for (Variable o : O) {
                 for (Table f : factors) {
                     if (o.equals(f.getNode()) || f.getParents().contains(o)) {
                         // remove instances of wrong values
                         int posO = 0;
-                        if(o.equals(f.getNode()))
-                            posO=o.getNrOfParents(); // if a node has two parents then its values are in col 2 of the table
-                        else
+                        if (o.equals(f.getNode())) {
+                            posO = o.getNrOfParents(); // if a node has two parents then its values are in col 2 of the table
+                        } else {
                             for (int i = 0; i < f.getParents().size(); i++) {
-                                if(o.equals(f.getParents().get(i)))
-                                        posO=i;                                                                 
+                                if (o.equals(f.getParents().get(i))) {
+                                    posO = i;
+                                }
                             }
+                        }
                         //posO done
                         ArrayList<ProbRow> table = f.getTable(); // does this work? removing rows from loop?
                         for (ProbRow row : table) {
-                            if(!row.getValues().get(posO).equals(o.getValue()))
-                               f.getTable().remove(row);
+                            if (!row.getValues().get(posO).equals(o.getValue())) {
+                                f.getTable().remove(row);
+                            }
                         }
                     }
                 }
             }
         }
+
         // e)
         ArrayList<Variable> elimOrder = new ArrayList<>();
-        for (Variable v : Vs) {
-            if(!v.hasParents())
-                break; //NOT RIGHT just to stop error.
+        ArrayList<Variable> parents = new ArrayList<>(); // make set?
+
+        for (Variable v : Vs) {     // get a list of parents
+            if (v.hasParents()) {
+                parents.addAll(v.getParents());
+            } else {
+                elimOrder.add(v); // add top nodes
+            }
         }
-    }    
+        System.out.println(parents);
+        for (Variable v : Vs) {
+            if (!parents.contains(v) && !v.equals(Q)) { //make sure you dont add the query variable
+                elimOrder.add(0, v);     // add leaf nodes at front of list
+            }
+        }
+        for (Variable v : Vs) {
+            if (!elimOrder.contains(v) && !v.equals(Q)) {
+                elimOrder.add(v);       // add remaining nodes (exc. query)
+            }
+        }
+
+        // f)
+        for (Variable z : elimOrder) {
+            ArrayList<Table> zFactors = new ArrayList<>(); // list of factors that contain z
+            for (Table factor : factors) {
+                if (factor.getNode().equals(z) || factor.getParents().contains(z)) {
+                    zFactors.add(factor);
+                }
+            }
+            // a)
+            if (zFactors.size() > 1) { // only multiply if theres more than one factor
+                Table biggest = zFactors.get(0);
+                for (Table zFactor : zFactors) { // find the biggest table
+                    if (zFactor.size() > biggest.size()) {
+                        biggest = zFactor;
+                    }
+                }
+                for (ProbRow row : biggest.getTable()) {
+                    
+                }
+            }
+        }
+    }
 }
